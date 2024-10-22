d49ddd4257355cd9f6743f2913c82046c2371bd7
serg473 <sergeidsi473@gmail.com>
2024-10-22 12:46:48 +0300

    feat: create component worklist

---
+++ src/components/WorkList.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useStore } from '@/stores/portfolio';
+import WorkListItem from './WorkListItem.vue';
+const store = useStore();
+</script>
+<template>
+    <div>
+        <WorkListItem v-for="work in store.work" :key="work.name" :workItem="work" />
+    </div>
+</template>
+<style></style>
